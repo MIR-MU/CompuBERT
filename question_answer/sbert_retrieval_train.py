3bd27860a91d4de772484035a4d257c266052840
Michal Štefánik <michal.stefanik@gaussalgo.com>
2020-02-17 18:22:16 +0100

    sbert_retrieval_train: Random sampler for eval dataset, eval_steps and warmup reconfig (1/10 steps)

--- question_answer/sbert_retrieval_train.py
+++ question_answer/sbert_retrieval_train.py
@@ -1,6 +1,10 @@
+# TODO: not the main train script - just for local tests. Go to ../sbert_retrieval_train.py
+
+from torch.utils.data import RandomSampler, DataLoader
+
 from ARQMathCode.post_reader_record import DataReaderRecord
-from question_answer.utils import examples_from_questions, dataloader_from_examples
-from sentence_transformers import SentenceTransformer, losses
+from question_answer.utils import examples_from_questions
+from sentence_transformers import SentenceTransformer, losses, SentencesDataset
 
 from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
 
@@ -17,11 +21,12 @@ examples_len = len(all_examples)
 # train_dev_test_split = (int(0.1*examples_len), int(0.2*examples_len))
 train_dev_test_split = (160, 320)
 
-train_loader = dataloader_from_examples(all_examples[:train_dev_test_split[0]], model,
-                                        batch_size=16, shuffle=True)
+train_data = SentencesDataset(all_examples[:train_dev_test_split[0]], model, show_progress_bar=True)
+train_loader = DataLoader(train_data, batch_size=8, shuffle=True)
 
-dev_loader = dataloader_from_examples(all_examples[train_dev_test_split[0]:train_dev_test_split[1]], model,
-                                      batch_size=16, shuffle=False)
+dev_data = SentencesDataset(all_examples[train_dev_test_split[0]:train_dev_test_split[1]], model, show_progress_bar=True)
+dev_sampler = RandomSampler(dev_data, replacement=True, num_samples=5)
+dev_loader = DataLoader(train_data, batch_size=8, sampler=dev_sampler)
 
 train_loss = losses.CosineSimilarityLoss(model=model)
 evaluator = EmbeddingSimilarityEvaluator(dev_loader, show_progress_bar=True, device=device)
@@ -29,7 +34,7 @@ evaluator = EmbeddingSimilarityEvaluator(dev_loader, show_progress_bar=True, dev
 model.fit(train_objectives=[(train_loader, train_loss)],
           evaluator=evaluator,
           epochs=10,
-          evaluation_steps=10,
+          evaluation_steps=1,
           warmup_steps=100,
           output_path="out",
           optimizer_params={'lr': 2e-5, 'eps': 1e-6, 'correct_bias': False})
--- sbert_retrieval_train.py
+++ sbert_retrieval_train.py
@@ -1,6 +1,8 @@
+from torch.utils.data import RandomSampler
+
 from ARQMathCode.post_reader_record import DataReaderRecord
 from question_answer.utils import examples_from_questions, dataloader_from_examples
-from sentence_transformers import SentenceTransformer, losses
+from sentence_transformers import SentenceTransformer, losses, SentencesDataset
 
 from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
 
@@ -21,12 +23,14 @@ examples_len = len(all_examples)
 
 train_dev_test_split = (int(0.8*examples_len), int(0.9*examples_len))
 
-model = SentenceTransformer('bert-base-wikipedia-sections-mean-tokens', logfile="train_whole.log")
+model = SentenceTransformer('bert-base-wikipedia-sections-mean-tokens', logfile="train_whole_sampled_eval.log")
 
-train_loader = dataloader_from_examples(all_examples[:train_dev_test_split[0]], model,
-                                        batch_size=16, shuffle=True)
-dev_loader = dataloader_from_examples(all_examples[train_dev_test_split[0]:train_dev_test_split[1]], model,
-                                      batch_size=16, shuffle=False)
+train_data = SentencesDataset(all_examples[:train_dev_test_split[0]], model, show_progress_bar=True)
+train_loader = DataLoader(train_data, batch_size=16, shuffle=True)
+
+dev_data = SentencesDataset(all_examples[train_dev_test_split[0]:train_dev_test_split[1]], model, show_progress_bar=True)
+dev_sampler = RandomSampler(dev_data, replacement=True, num_samples=250)
+dev_loader = DataLoader(train_data, batch_size=16, sampler=dev_sampler)
 
 train_loss = losses.CosineSimilarityLoss(model=model)
 evaluator = EmbeddingSimilarityEvaluator(dev_loader, show_progress_bar=True, device=device)
@@ -34,8 +38,8 @@ evaluator = EmbeddingSimilarityEvaluator(dev_loader, show_progress_bar=True, dev
 model.fit(train_objectives=[(train_loader, train_loss)],
           evaluator=evaluator,
           epochs=3,
-          evaluation_steps=100,
-          warmup_steps=100000,
-          output_path="out_whole",
+          evaluation_steps=2000,
+          warmup_steps=int(217206/10),
+          output_path="out_whole_sampled_eval",
           optimizer_params={'lr': 2e-5, 'eps': 1e-6, 'correct_bias': False})
 
