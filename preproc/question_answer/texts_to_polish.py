bfc27d50eba3e7530c816e20495d7ae7ef1dce4b
Michal Štefánik <michal.stefanik@gaussalgo.com>
2020-03-02 20:39:04 +0100

    Stupid and slow polish preproc

--- preproc/question_answer/texts_to_polish.py
+++ preproc/question_answer/texts_to_polish.py
@@ -1,10 +1,39 @@
 from ARQMathCode.post_reader_record import DataReaderRecord
 from functools import reduce
 
+from TangentS.math_tan.math_extractor import MathExtractor
 
-def tex_to_polish(tex_formula):
-    # TODO
-    return tex_formula
+alias_map = {"eq": "equals",
+             "sup": "powered",
+             "log": "logarithm",
+             "ln": "natural logarithm",
+             "sqrt": "squared root"}
+
+
+def alias(val, is_var=False):
+    if is_var:
+        return "variable %s" % val
+    return alias_map.get(val.lower(), val)
+
+
+def latex_to_polish_list(expression):
+    seen_ops = set()
+    for transition in MathExtractor.parse_from_tex_opt(expression).get_pairs(window=1, eob=True):
+        trans_tuple = transition.split("\t")
+        # is var
+        if trans_tuple[0].startswith("V"):
+            yield alias(trans_tuple[0].split("!")[1], is_var=True)
+            continue
+        # trans_tuple[0]+trans_tuple[-1] is presumed to be op's unique ID in OPTree
+        if trans_tuple[0] + trans_tuple[-1] not in seen_ops:
+            yield alias(trans_tuple[0].split("!")[1])
+            seen_ops.add(trans_tuple[0] + trans_tuple[-1])
+        else:
+            seen_ops.remove(trans_tuple[0] + trans_tuple[-1])
+
+
+def latex_to_polish(expression):
+    return ' '.join(latex_to_polish_list(expression))
 
 
 def fulltext_to_polish(entity_body_text, formula_sep="$"):
@@ -14,7 +43,7 @@ def fulltext_to_polish(entity_body_text, formula_sep="$"):
     if len(formulas) == 0:
         return entity_body_text
     parts = [part for i, part in enumerate(body_parted) if i % 2 == 0]
-    formulas_polish = list(map(tex_to_polish, formulas)) + [""]
+    formulas_polish = list(map(latex_to_polish, formulas)) + [""]
     body_replaced_l = list(reduce(lambda x, part_tups: x + list(part_tups), zip(parts, formulas_polish), []))
     return formula_sep.join(body_replaced_l)
 
